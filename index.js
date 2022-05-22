8cef180759d37383a94d723493f0f31b0c88b231
DFeliciano3 <91075867+DFeliciano3@users.noreply.github.com>
2022-05-22 00:40:09 -0400

    commit tree df972626c22d462966a86d5402f902fe58a5123a

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(Ralph){
+    cats.push(Ralph);
+}
+
+function destructivelyPrependCat(Bob){
+    cats.unshift(Bob);
+}
+
+function destructivelyRemoveLastCat(Garfield){
+    cats.pop(Garfield)
+}
+
+function destructivelyRemoveFirstCat(Milo){
+    cats.shift(Milo)
+}
+
+function appendCat(Broom){
+    const catsCopy = [...cats];
+    catsCopy.push(Broom)
+    return catsCopy
+}
+
+function prependCat(Arnold) {
+    const catsCopy = [...cats]
+    catsCopy.unshift(Arnold)
+    return catsCopy
+}
+
+function removeLastCat(Garfield){
+    const catsCopy = [...cats]
+    catsCopy.pop(Garfield)
+    return catsCopy
+}
+
+function removeFirstCat(Milo){
+    const catsCopy = [...cats]
+    catsCopy.shift(Milo)
+    return catsCopy
+}
